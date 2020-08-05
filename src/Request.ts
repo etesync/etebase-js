42490bcb9a9ee70341561873aba5a77408afa7a5
Tom Hacohen <tom@stosb.com>
2020-08-05 10:09:03 +0300

    Revert "Node-fetch: fix loading of peer dep to be optional."
    This reverts commit f64d0abd79bcc9d67c76920c3f5bb359dd17d02f.

--- src/Request.ts
+++ src/Request.ts
@@ -47,13 +47,7 @@ async function request(url: string, options: RequestInit = {}): Promise<Response
 }
 
 async function requestNode(url: string, options: RequestInit = {}): Promise<Response> {
-  let fetch;
-  try {
-    fetch = await require("node-fetch");
-  } catch (e) {
-    console.error("Failed loading needed node-fetch peer dep.");
-    throw e;
-  }
+  const fetch = await require("node-fetch");
   const response = await fetch(url, options as any);
   const ret = {
     type: "default",
