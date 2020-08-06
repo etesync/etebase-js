496b67ce8244d0c5fea41fe5b67c1b4104bbd8ab
Tom Hacohen <tom@stosb.com>
2020-08-06 09:50:54 +0300

    Fix webpack missing import issue.

--- src/optional-require.ts
+++ src/optional-require.ts
@@ -1,3 +1,8 @@
-const optionalRequire = (global as any).__non_webpack_require__ ?? require;
+if ((global as any).__non_webpack_require__ === undefined) {
+  // eslint-disable-next-line
+  (global as any).__non_webpack_require__ = require;
+}
+
+const optionalRequire = (global as any).__non_webpack_require__;
 
 export default optionalRequire;
